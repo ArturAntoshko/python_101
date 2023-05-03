40cb0f47bdd55e0cb7837b9a98970a1e62981bf9
ArturAntoshko <kresha@ukr.net>
2023-05-03 15:47:31 +0300

    solved 2

---
+++ if-else/task_2.py
@@ -0,0 +1,23 @@
+number = int(input("Введите число от 1 до 10: "))
+if number == 1:
+    print("one")
+elif number == 2:
+    print("two")
+elif number == 3:
+    print("three")
+elif number == 4:
+    print("four")
+elif number == 5:
+    print("five")
+elif number == 6:
+    print("six")
+elif number == 7:
+    print("seven")
+elif number == 8:
+    print("eight")
+elif number == 9:
+    print("nine")
+elif number == 10:
+    print("ten")
+else:
+    print("Ошибка, введите число от 1 до 10")
---
+++ if-else/task_3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+num3 = int(input("Введите третье число: "))
+
+max_num = max(num1, num2, num3)
+
+print(f"Максимальное число: {max_num}")
